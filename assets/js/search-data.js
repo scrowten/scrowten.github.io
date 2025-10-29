939fa7f28203714cca16633678ae34e6398c7e94
scrowten <rizkyagung22@gmail.com>
2025-10-29 05:19:53 +0000

    Deploying to gh-pages from @ scrowten/scrowten.github.io@6158e3c61632ecfadd75ddfce0959e64e467b986 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,14 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-starting-a-new-position-as-data-scientist-at-tokopedia",
+          title: 'Starting a new position as Data Scientist at Tokopedia! 🎉',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-graduated-from-universitas-gadjah-mada-majoring-in-computer-science",
+          title: 'Graduated from Universitas Gadjah Mada majoring in Computer Science.',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-starting-a-new-position-as-algorithm-engineer-at-bytedance",
+          title: 'Starting a new position as Algorithm Engineer at ByteDance! 🎉',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
@@ -501,14 +499,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%72%69%7A%6B%79%61%67%75%6E%67%32%32@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/scrowten", "_blank");
+        },
+      },{
+        id: 'social-kaggle',
+        title: 'Kaggle',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.kaggle.com/rizkyagung22", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/rizky-agung-dwi-putranto", "_blank");
         },
       },{
         id: 'social-rss',
@@ -516,20 +528,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
