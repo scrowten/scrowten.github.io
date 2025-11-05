b6e62bc60dc0fc8da7765f0a841524b82c4eac46
scrowten <rizkyagung22@gmail.com>
2025-11-05 05:04:38 +0000

    Deploying to gh-pages from @ scrowten/scrowten.github.io@fcea07d05019b1d5ee4557f2104cf3ac3f1acade 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,9 +37,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+        },{id: "nav-rizky-agung-dwi-putranto",
+          title: "Rizky Agung Dwi Putranto",
+          description: "Curriculum vitae of Rizky Agung Dwi Putranto — Machine Learning / NLP / Computer Vision engineer.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
